a24e22c685925143f8bbd01e5e9982d67035b268
solong1980 <solong1980@163.com>
2018-05-15 22:54:04 +0800

    漏了

--- goodscm-service/src/main/java/springboot/shiro/MyShiroRealm.java
+++ goodscm-service/src/main/java/springboot/shiro/MyShiroRealm.java
@@ -1,7 +1,5 @@
 package springboot.shiro;
 
-import javax.annotation.Resource;
-
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -13,17 +11,22 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.xlw.goodscm.model.User;
-import com.xlw.goodscm.service.UserService;
+import com.xlw.sys.dao.SysMenuMapper;
+import com.xlw.sys.dao.SysUserMapper;
+import com.xlw.sys.model.SysUser;
 
 /**
  * Created by Administrator on 2017/12/11. 自定义权限匹配和账号密码匹配
  */
 public class MyShiroRealm extends AuthorizingRealm {
 	private static final Logger logger = LoggerFactory.getLogger(MyShiroRealm.class);
-	@Resource
-	private UserService userService;
+	@Autowired
+	private SysUserMapper sysUserMapper;
+	@Autowired
+	private SysMenuMapper sysMenuMapper;
 
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
@@ -45,21 +48,23 @@ public class MyShiroRealm extends AuthorizingRealm {
 		// System.out.println("MyShiroRealm.doGetAuthenticationInfo()");
 		logger.info("doGetAuthenticationInfo");
 		// 获取用户的输入的账号.
-		String account = (String) token.getPrincipal();
+		String username = (String) token.getPrincipal();
 		// System.out.println(token.getCredentials());
 		// 通过account从数据库中查找 User对象，如果找到，没找到.
 		// 实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
-		User userInfo = userService.findByAccount(account);
+		SysUser user = new SysUser();
+		user.setUsername(username);
+		user = sysUserMapper.selectOne(user);
 		// System.out.println("----->>userInfo="+userInfo);
-		if (userInfo == null) {
+		if (user == null) {
 			return null;
 		}
 		// if (userInfo.getState() == 1) { // 账户冻结
 		// throw new LockedAccountException();
 		// }
-		SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(userInfo, // 用户名
-				userInfo.getPassword(), // 密码
-				ByteSource.Util.bytes(userInfo.getCredentialsSalt()), // salt=username+salt
+		SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(user, // 用户名
+				user.getPassword(), // 密码
+				ByteSource.Util.bytes(user.getSalt().getBytes()), // salt=username+salt
 				getName() // realm name
 		);
 		return authenticationInfo;
