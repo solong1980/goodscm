594830178fbe6e93da771e16ba29943d933593ec
solong1980 <solong1980@163.com>
2018-06-08 18:39:52 +0800

    1.供应商增加新属性

--- goodscm-service/src/main/java/com/xlw/goodscm/model/Supplier.java
+++ goodscm-service/src/main/java/com/xlw/goodscm/model/Supplier.java
@@ -3,74 +3,188 @@ package com.xlw.goodscm.model;
 import java.util.Date;
 import java.util.List;
 
-/**
- * 供应商
- * 
- * @author solon
- *
- */
 public class Supplier {
-	private Long id;
+    private Long id;
 
-	private Long groupId;
+    private Long groupId;
 
-	private String name;
+    private String name;
 
-	private String code;
+    private String code;
 
-	private Date createTime;
+    private String contacts;
 
-	private Date updateTime;
+    private String mobile;
+
+    private String phone;
+
+    private String fax;
+
+    private String qq;
+
+    private String wechat;
+
+    private String email;
+
+    private String website;
+
+    private String address;
+
+    private Boolean status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String description;
+
+    private String memo;
 
 	private List<Goods> supplierGoods;
+    
+    public Long getId() {
+        return id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getGroupId() {
+        return groupId;
+    }
 
-	public Long getGroupId() {
-		return groupId;
-	}
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
 
-	public void setGroupId(Long groupId) {
-		this.groupId = groupId;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
 
-	public void setName(String name) {
-		this.name = name == null ? null : name.trim();
-	}
+    public String getCode() {
+        return code;
+    }
 
-	public String getCode() {
-		return code;
-	}
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
 
-	public void setCode(String code) {
-		this.code = code == null ? null : code.trim();
-	}
+    public String getContacts() {
+        return contacts;
+    }
 
-	public Date getCreateTime() {
-		return createTime;
-	}
+    public void setContacts(String contacts) {
+        this.contacts = contacts == null ? null : contacts.trim();
+    }
 
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
+    public String getMobile() {
+        return mobile;
+    }
 
-	public Date getUpdateTime() {
-		return updateTime;
-	}
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
 
-	public void setUpdateTime(Date updateTime) {
-		this.updateTime = updateTime;
-	}
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax == null ? null : fax.trim();
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq == null ? null : qq.trim();
+    }
+
+    public String getWechat() {
+        return wechat;
+    }
+
+    public void setWechat(String wechat) {
+        this.wechat = wechat == null ? null : wechat.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website == null ? null : website.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo == null ? null : memo.trim();
+    }
 
 	public List<Goods> getSupplierGoods() {
 		return supplierGoods;
@@ -79,5 +193,6 @@ public class Supplier {
 	public void setSupplierGoods(List<Goods> supplierGoods) {
 		this.supplierGoods = supplierGoods;
 	}
-
+    
+    
 }
