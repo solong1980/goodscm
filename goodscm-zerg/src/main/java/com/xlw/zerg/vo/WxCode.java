8d0b46e361848cfa61b6c7a52acb60d57f82eacd
solong1980 <solong1980@163.com>
2018-09-06 14:15:48 +0800

    add miss code

---
+++ goodscm-zerg/src/main/java/com/xlw/zerg/vo/WxCode.java
@@ -0,0 +1,17 @@
+package com.xlw.zerg.vo;
+
+import java.io.Serializable;
+
+public class WxCode implements Serializable{
+	private static final long serialVersionUID = 5696220200891926943L;
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+}
